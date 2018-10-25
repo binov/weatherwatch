f57373c1a425071df4028d08aad0b2c324f7fbe8
Bino Varghese <binovarghese85@gmail.com>
2018-10-25 23:35:51 +0530

    bug fix

--- client/src/reducers/reducer_weather.js
+++ client/src/reducers/reducer_weather.js
@@ -2,7 +2,9 @@ import { FETCH_WEATHER, FETCH_WEATHER_ALL } from "../actions/index.js";
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return [...action.payload.data.city_list, ...state];
+      return action.payload.data
+        ? [...action.payload.data.city_list, ...state]
+        : state;
     case FETCH_WEATHER_ALL:
       return action.payload.data ? [...action.payload.data.city_list] : [];
     default:
