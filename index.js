0afa623fa36166ce41ebb19eef6bd34ab2838a59
Bino Varghese <binovarghese85@gmail.com>
2018-10-28 18:41:05 +0530

    adding heroku configurations

--- index.js
+++ index.js
@@ -16,5 +16,16 @@ app.use(bodyParser.json());
 
 weatherRoutes(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve the production assets
+  app.use(express.static("client/build"));
+  // Express will serve up the index.html file,
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
